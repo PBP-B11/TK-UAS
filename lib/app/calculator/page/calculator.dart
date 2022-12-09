f506aef624642467d009b613092fcd33f825e009
rafimadani <rafi.madani@ui.ac.id>
2022-12-10 00:42:51 +0700

    biar ada progress dikit

--- lib/app/calculator/page/calculator.dart
+++ lib/app/calculator/page/calculator.dart
@@ -12,12 +12,95 @@ class CalculatorPage extends StatefulWidget {
 class _CalculatorPageState extends State<CalculatorPage> {
   @override
   Widget build(BuildContext context) {
+    final PageController controller = PageController();
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Calculator'),
       ),
-      body: Center(),
       drawer: MyDrawer(),
+      body: Form(
+      child: PageView(
+      /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+      /// Use [Axis.vertical] to scroll vertically.
+      controller: controller,
+      children: <Widget>[
+        Center(
+          child: Text('According to the U.S. Energy Information Administration, the average household uses around 893 kilowatt-hours (kWh) per month. A residential solar setup produces anywhere from 350 to 850 kWh per month. Therefore, you can save as much as 95% off your utility bill.'),
+        ),
+        Center(
+          child:  Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Tagihan Listrik Selama 1 Tahun (KWH)",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  )),
+        ),
+          ),
+        ),
+      Center(
+        child:  Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Persentase Tagihan Yang Dicover Solar Panel (%)",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                )),
+          ),
+        ),
+      ),
+        Center(
+          child:  Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Faktor Pengaruh Cuaca",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  )),
+            ),
+          ),
+        ),
+        Center(
+          child:  Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Luas Perkiraan Atap Rumah (M^2)",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  )),
+            ),
+          ),
+        ),
+        Center(
+          child:  TextButton(
+            style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.all(20.0),
+            ),
+          ),
+            onPressed:null ,
+            child: const Text(
+              "Cek Hasil Kalkulasi Anda",
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ),
+      ],
+      ),
+      )
     );
+
+
+
   }
 }
